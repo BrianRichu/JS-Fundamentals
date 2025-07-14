550eaeb9a577c53e9f3bdbe93230034b6e06cbfa
Brian Richu <richubrian427@gmail.com>
2025-07-14 09:14:21 +0300

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -2,7 +2,7 @@ const myArray = ["C is fun", "Python is cool", "JavaScript is amazing"];
 let result = "";
 
 for (let index = 0; index < myArray.length; index++) {
-  result += myArray[index];
+  result += myArray[index] + "\n" ;
   
 }
 console.log(result);
