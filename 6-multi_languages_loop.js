4ffb204d335e79a0bb8b5188f867c392ac9e1553
Brian Richu <richubrian427@gmail.com>
2025-07-14 09:05:18 +0300

    Create 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+const myArray = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let result = "";
+
+for (let index = 0; index < myArray.length; index++) {
+  result += myArray[index];
+  
+}
+console.log(result);
