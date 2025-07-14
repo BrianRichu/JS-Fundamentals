ed8a1385c5cf0d20038bc997ba7eea84ae47aa51
Brian Richu <richubrian427@gmail.com>
2025-07-14 08:08:50 +0300

    Create 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const args = [];
+if (args.length===0) {
+  console.log("No argument");
+}else if (args.length===1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguements found")
+}
